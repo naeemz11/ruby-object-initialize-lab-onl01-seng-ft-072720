207c65c135346de543065980db42d7083267f511
Zannatul Naeem <mollahnaeem@gmail.com>
2020-08-25 18:47:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,16 +1,8 @@
 class Dog 
   
-  def initialize(name, breed="Mutt")
-    @name= name 
-    @breed= breed
-  end 
   
-  def name 
-    @name 
-  end 
   
-  def breed
-    @breed 
-  end 
+  
+  
   
 end 
